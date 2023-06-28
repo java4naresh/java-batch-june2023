03b4d41bf35c092c10c2a403d484651c0f7818b9
srilatha <srilathabangaru14@gmail.com>
2023-06-28 21:16:51 +0530

    IntPrcatice

---
+++ Srilatha/IntExample.java
@@ -0,0 +1,6 @@
+class IntExample{
+public static void main(String args[]){
+int dressprice=56666000;
+System.out.println("My dressprice is" +dressprice);
+}
+}
---
+++ Srilatha/Practice.java
@@ -0,0 +1,8 @@
+class Practice{
+public static void main(String args[])
+{
+System.out.println("My name is srilatha");
+System.out.println("Shamu is my friend");
+System.out.println("we completed m.sc");
+}
+}
