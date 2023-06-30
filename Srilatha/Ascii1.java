f411cc9434eca24dd3e0b846baaec8a71cd43482
srilatha <srilathabangaru14@gmail.com>
2023-06-30 19:42:42 +0530

    AsciiExample

---
+++ Srilatha/Ascii1.java
@@ -0,0 +1,10 @@
+class Ascii1{
+public static void main(String args[])
+{
+char gender = 'F';
+char asciiValue = 67;
+
+System.out.println("My gender is"+gender);
+System.out.println(asciiValue);
+}
+}
---
+++ Srilatha/AsciiExample.java
@@ -0,0 +1,44 @@
+class Ascii1{
+public static void main(String args[])
+{
+char gender = 'F';
+char asciiValue = 67;
+
+System.out.println("My gender is"+gender);
+System.out.println(asciiValue);
+}
+}
+
+class AsciiExample{
+public static void main(String args[])
+{
+char s='S';
+char r='R';
+char i='I';
+char l='L';
+char a='A';
+char t='T';
+char h='H';
+
+char asciiValue=55;
+char asciiValue1=5;
+char asciiValue2=7;
+char asciiValue3=16;
+char asciiValue4=34;
+char asciiValue5=65;
+char asciiValue6=78;
+
+
+System.out.println("s");
+System.out.println("r");
+System.out.println("i");
+System.out.println("l");
+System.out.println("a");
+System.out.println("t");
+System.out.println("h");
+
+
+System.out.println("asciiValue +asciiValue1 +asciiValue2 +asciiValue3 +asciiValue4 +asciiValue5 +asciiValue6\n");
+
+}
+}
