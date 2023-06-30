23f62c925b96927405edf71071f17f6341dddd8f
srilatha <srilathabangaru14@gmail.com>
2023-06-30 14:24:40 +0530

    DataTypesPractice

---
+++ Srilatha/DataTypes.java
@@ -0,0 +1,22 @@
+class DataTypes{
+public static void main(String args[])
+{
+byte byteValue=99;
+short shortValue=999;
+int intValue=999999;
+long longValue=99999999999L;
+float floatValue=6.18f;
+double doubleValue=9.999999999999;
+boolean booleanValue=true;
+char charValue='s';
+
+System.out.println("byte Value is" +byteValue);
+System.out.println("shortValue is"+shortValue);
+System.out.println("intValue is"+intValue);
+System.out.println("longValue is"+longValue);
+System.out.println("floatValue is"+floatValue);
+System.out.println("doubleValue is"+doubleValue);
+System.out.println("booleanValue is"+booleanValue);
+System.out.println("charValue is"+charValue);
+}
+}
