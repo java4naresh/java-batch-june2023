123626d4a1908eb4a8a572593e20c61153316b21
srilatha <srilathabangaru14@gmail.com>
2023-07-03 19:29:03 +0530

    OperatorsExample

---
+++ Srilatha/ArthematicOperaters.java
@@ -0,0 +1,18 @@
+class ArthematicOperaters{
+public static void main(String args[])
+{
+ int x=5;
+ int y=5;
+ 
+int sum=x+y;
+int difference=x-y;
+int product=x*y;
+int quotient=x/y;
+int remainder=x%y;
+System.out.println("Sum of two numbers is "+sum);
+System.out.println("Difference of two numbers is "+difference);
+System.out.println("Product of two numbers is "+product);
+System.out.println("Quotient of two numbers is "+quotient);
+System.out.println("Remainder of two numbers is "+remainder);
+}
+}
