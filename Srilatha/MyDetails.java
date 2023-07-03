dc7e00cf6f208b3771bbd66765a0a0638a94cef0
srilatha <srilathabangaru14@gmail.com>
2023-07-03 20:14:39 +0530

    StringExample

---
+++ Srilatha/MyDetails.java
@@ -0,0 +1,25 @@
+class MyDetails{
+public static void main(String args[])
+{
+String firstname="Srilatha";
+String lastname="";
+String surname="Bangaru";
+String dob="15-12-1999";
+String fathersname="Saidachari";
+String mothersname="Leelavathi";
+String goal="javaDeveloper";
+boolean physicallyDisabled=false;
+short age=23;
+
+System.out.println("my firstname is"+firstname);
+System.out.println("my lastname is"+lastname);
+System.out.println("my surname is"+surname);
+System.out.println("my dob is"+dob);
+System.out.println("my fathersname is"+fathersname);
+System.out.println("mothersname is"+mothersname);
+System.out.println("my goal is"+goal);
+System.out.println("Am i physicallyDisabled"+physicallyDisabled);
+System.out.println("my age is"+age);
+
+}
+}
