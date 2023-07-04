b91da07442a774f2aad0975a1c99ac34ab5723d7
java4naresh <kambalanaresh4java@gmail.com>
2023-07-04 20:30:34 +0530

    ATM Program

---
+++ Naresh/IfElseExample.java
@@ -0,0 +1,23 @@
+class IfElseExample 
+{
+	public static void main(String[] args) 
+	{
+		int myAge = 20;
+		if(myAge > 18) {
+		  System.out.println("you are Major");
+		} /*else {
+		  System.out.println("you are Minor");
+		}*/
+
+		if(myAge > 60) {
+		  System.out.println("you are Senior Citizen");
+		}
+		
+
+		else {
+		  System.out.println("you are not a Senior Citizen");
+		}
+
+
+	}
+}
---
+++ Naresh/IfExample.java
@@ -0,0 +1,19 @@
+class IfExample {
+
+public static void main(String[] args) {
+// if condition example
+char gender = 'C';
+if(gender == 'F') {
+System.out.println("Your gender is Female");
+}
+
+if(gender == 'M') {
+System.out.println("Your gender is Male");
+}
+
+if(gender != 'M' && gender != 'F') {
+System.out.println("Your gender is Transgender");
+}
+
+}
+}
