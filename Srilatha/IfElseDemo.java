657aed4053f31c856474c6b486826490b1de8b95
srilatha <srilathabangaru14@gmail.com>
2023-07-05 21:14:59 +0530

    ExamplesIfElse

---
+++ Srilatha/IfElseDemo.java
@@ -0,0 +1,17 @@
+class IfElseDemo{
+public static void main(String args[])
+{
+     int myAge=20;
+     if(myAge>18){
+     System.out.println("you are major and you are eligible to vote");
+     }else{
+      System.out.println("you are minor and you are not eligible to vote");
+     
+      }
+      if(myAge==18){
+      System.out.println("Welcome you are  not eligible to vote");
+      }else{
+      System.out.println("Thankyou");
+      }
+}
+}
