5d933a98fc5a02a51c52dd1ccc0e69a8dc27e892
java4naresh <kambalanaresh4java@gmail.com>
2023-07-05 20:00:47 +0530

    Elseif ladder and switch case examples

---
+++ Naresh/IfElseLadder.java
@@ -0,0 +1,71 @@
+class IfElseLadder 
+{
+	public static void main(String[] args) 
+	{
+		int day = 1;
+		/*if(day == 1) {
+		System.out.println("Today is Sunday");
+		}
+		else if(day == 2) {
+		System.out.println("Today is Monday");
+		} else if(day == 3) {
+		System.out.println("Today is Tuesday");
+		} else if(day == 4) {
+		System.out.println("Today is Wednesday");
+		} else if(day == 5) {
+		System.out.println("Today is Thursday");
+		} else if(day == 6) {
+		System.out.println("Today is Friday");
+		} else if(day == 7) {
+		System.out.println("Today is Saturday");
+		} else {
+			System.out.println("Invalid");
+		}*/
+       /* if(day == 1) {
+		System.out.println("Today is Sunday");
+		}
+		if(day == 2) {
+		System.out.println("Today is Monday");
+		} 
+		if(day == 3) {
+		System.out.println("Today is Tuesday");
+		} 
+		if(day == 4) {
+		System.out.println("Today is Wednesday");
+		} 
+		if(day == 5) {
+		System.out.println("Today is Thursday");
+		} 
+		if(day == 6) {
+		System.out.println("Today is Friday");
+		} 
+		if(day == 7) {
+		System.out.println("Today is Saturday");
+		} 
+		if(day > 7) {
+			System.out.println("Invalid");
+		}*/
+		/*int age = 18;
+		if(age <= 12) {
+		System.out.println("Child");
+		} else if(age <= 19) {
+		System.out.println("Teenage");
+		}  else if(age <= 60) {
+		System.out.println("Youngage");
+		} else {
+		System.out.println("Oldage");
+		}*/
+
+       int age = 18;
+		if(age <= 12) {
+		System.out.println("Child");
+		} if(age <= 19) {
+		System.out.println("Teenage");
+		}  if(age <= 60) {
+		System.out.println("Youngage");
+		} else {
+		System.out.println("Oldage");
+		}
+
+	}
+}
