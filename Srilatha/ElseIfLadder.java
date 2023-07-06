e0911d761f633ae4e3e485738d8b66ae198f889d
srilatha <srilathabangaru14@gmail.com>
2023-07-06 17:31:52 +0530

    Vowel

---
+++ Srilatha/ElseIfLadder.java
@@ -0,0 +1,32 @@
+class ElseIfLadder{
+public static void main(String args[])
+{
+     int Vowel=11;
+     if(Vowel==1) {
+     	 System.out.println('A');
+	 }else if(Vowel==2){
+         System.out.println('E');
+	 }else if(Vowel==3){
+	 System.out.println('I');
+	 }else if(Vowel==4){
+	 System.out.println('O');
+	 }else if(Vowel==5){
+	 System.out.println('U');
+	 }else if(Vowel==6){
+	 System.out.println('a');
+	 }else if(Vowel==7){
+	 System.out.println('e');
+	 }else if(Vowel==8){
+	 System.out.println('i');
+	 }else if(Vowel==9){
+	 System.out.println('o');
+	 }else if(Vowel==10){
+	 System.out.println('u');
+	 }else{
+	 System.out.println("Consonant");
+}
+	
+      
+    
+}
+}
---
+++ Srilatha/IfElse3.java
@@ -0,0 +1,24 @@
+class IfElse3{
+public static void main(String args[])
+{
+int day=5;
+if(day==1)
+ {  
+     System.out.println("Monday");
+ }if(day==2){
+     System.out.println("Tuesday");
+ }if(day==3){
+     System.out.println("Wednesday");
+ }if(day==4){
+     System.out.println("Thursday");
+ }if(day==5){
+     System.out.println("Friday");
+ }if(day==6){
+     System.out.println("Saturday");
+ }if(day==7){
+     System.out.println("Sunday");
+ }else{
+     System.out.println("Invalid");
+ }
+}
+}
