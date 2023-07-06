65e51bfb17e6947cf3d65f1654a4be01fd1ea8bb
Syamala02 <tellurisyamala@gmail.com>
2023-07-06 20:11:01 +0530

    ATMProgram.java,IfElseLadderExample.java practice

---
+++ Shyamala/ATMProgram.java
@@ -0,0 +1,21 @@
+class ATMProgram
+{
+public static void main(String[] args)
+{
+    int actualPinNo=1234;
+    int enteredPinNo=1234;
+    double enteredwithdrawamount=5000;
+    double availableBalance=5000;
+  if(actualPinNo=enteredPinNO){
+     System.out.println("welcome to HDFC ATM!");
+  if(enteredwithdrawamount<=availableBalance){
+    if(enteredwithdrawamount)
+     System.out.println("Take your amount");
+}else{
+     System.out.println("Insuficient Funds");
+}
+}else{
+     System.out.println("entered pin number is wrong!");
+}
+}
+}
---
+++ Shyamala/IfElseLadderExample.java
@@ -0,0 +1,20 @@
+class IfElseLadderExample
+{
+public static void main(String[] args)
+{
+   int vowel=4; 
+   if(vowel==1){
+     System.out.println("a is vowel");
+     }else if(vowel==2){
+     System.out.println("e is vowel");
+     }else if(vowel==3){
+     System.out.println("i is vowel"); 
+     }else if(vowel==4){
+     System.out.println("o is vowel");
+     }else if(vowel==5){
+     System.out.println("u is vowel");
+     }else{
+     System.out.println("consonant");
+}
+}
+}               
