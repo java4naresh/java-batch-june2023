d082a36121d9d9079b9c08626a50bdda1f6c6e96
srilatha <srilathabangaru14@gmail.com>
2023-07-07 18:40:27 +0530

    VowelConsonant.java

---
+++ Srilatha/VowelConsonant.java
@@ -0,0 +1,10 @@
+class VowelConsonant{
+	public static void main(String args[]){
+	char ch='a';
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+	System.out.println("vowel");
+	}else{
+	System.out.println("Consonant");
+	}
+}
+}
