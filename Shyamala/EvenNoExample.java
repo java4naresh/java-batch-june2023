240124e6e093e78abd8ad6ffc022529c26955c9b
Syamala02 <tellurisyamala@gmail.com>
2023-07-07 18:40:59 +0530

    EvenNoExample.java practic

---
+++ Shyamala/EvenNoExample.java
@@ -0,0 +1,10 @@
+class EvenNoExample{
+public static void main(String []args){
+  int no=10;
+  if(no%2==0){
+    System.out.println("given no is even number");
+  }else {
+   System.out.println("given no is odd number");
+}
+}
+}
