50ebf721135098f7cbcca4bf955f9bf4f9160e87
srilatha <srilathabangaru14@gmail.com>
2023-07-20 18:58:01 +0530

    ObjectControlFlow3practice

---
+++ Srilatha/ObjectControlFlow3.java
@@ -0,0 +1,26 @@
+class ObjectControlFlow3
+{
+	int number=30;
+	int number2;
+
+	{
+	System.out.println("object is created");
+	}
+
+	public ObjectControlFlow3(int number,int number2){
+	System.out.println(this.number);
+	System.out.println(this.number2);
+	this.number=number;
+	this.number2=number2;
+	System.out.println(this.number);
+	System.out.println(this.number2);
+}
+
+
+	public static void main(String args[])
+	{
+	
+	ObjectControlFlow3 ocf3 = new ObjectControlFlow3(1,4);
+	ObjectControlFlow3 ocf4 = new ObjectControlFlow3(16,15);
+	}
+}
