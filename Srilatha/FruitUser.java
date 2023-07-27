5b73f5e1ee8ffbd6eb26a066cf371974d35699bf
srilatha <srilathabangaru14@gmail.com>
2023-07-27 18:36:52 +0530

    InheritancePractice2

--- Srilatha/FruitUser.java
+++ Srilatha/FruitUser.java
@@ -4,14 +4,18 @@ public class FruitUser
 
 	Fruits f=new Fruits();
 	f.printFruit1Name();
+	System.out.println (f.FruitName);
+
 
 	Fruits2 f2=new Fruits2();
 	f.printFruit1Name();
 	f2.printFruit2Name();
+	System.out.println (f2.FruitName);
 
 	Fruits3 f3=new Fruits3();
 	f.printFruit1Name();
 	f2.printFruit2Name();
 	f3.printFruit3Name();
+	System.out.println (f3.FruitName);
 	}
 }
--- Srilatha/Fruits.java
+++ Srilatha/Fruits.java
@@ -1,5 +1,7 @@
 public class Fruits{
 
+String FruitName="Apple";
+
 public void printFruit1Name(){
 System.out.println("printClassFruits");
 }
