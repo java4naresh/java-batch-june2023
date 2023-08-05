d02b9f144167d9b69dfb79f5eaec763aa3fd45d2
srilatha <srilathabangaru14@gmail.com>
2023-08-05 18:26:10 +0530

    OverLoading2

---
+++ Srilatha/SuperPower.java
@@ -0,0 +1,25 @@
+public class SuperPower{
+
+public String god(){
+return "I love Lord Krishna";
+}
+
+public int god(int num){
+return num;
+}
+
+public String god(int num1, String name1){
+return  num1+name1;
+}
+
+public String god(int num2, String name2, int num3, String name3){
+return num2+name2+num3+name3;
+}
+
+
+public String god(int num4, String name4, int num5, String name5, int num6, String name6,int num7, String name7, int num8, String name8, int num9, String name9, int num10, String name10){
+return num4+name4 +num5+name5 +num6+name6 +num7+name7 +num8+name8 +num9+name9 +num10+name10;
+}
+
+
+}
---
+++ Srilatha/VishnuAvtars.java
@@ -0,0 +1,10 @@
+public class VishnuAvtars{
+
+public static void main(String args[]){
+SuperPower Sp=new SuperPower();
+System.out.println(Sp.god(1, "Matsya"));
+System.out.println(Sp.god(2, "Kurma" , 3, "Varaha"));
+System.out.println(Sp.god(4, "Narasimha" , 5, "Parasurama" ,6, "Vamana", 7, "Rama" , 8, "Krishna" , 9, "Buddha" , 10, "kalki"));
+System.out.println(Sp.god());
+}
+}
