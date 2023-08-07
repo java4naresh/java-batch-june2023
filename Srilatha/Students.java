3814968787cd77690e9845ea51bd8c496b3bf31c
srilatha <srilathabangaru14@gmail.com>
2023-08-07 18:28:52 +0530

    OverridingExample

---
+++ Srilatha/Students.java
@@ -0,0 +1,11 @@
+public class Students extends PTMaster{
+
+public void doActivityonGame(){
+System.out.println("i got a prize in koo-koo");
+}
+
+public void duty(){
+System.out.println("my duty is to learn");
+
+  }
+  }
---
+++ Srilatha/StudentsPTMasterClient.java
@@ -0,0 +1,12 @@
+public class StudentsPTMasterClient {
+ public static void main(String[] args){
+   Students srilatha = new Students();
+      srilatha.doActivityonGame();
+	srilatha.duty();
+
+	PTMaster pt=new PTMaster();
+	pt.doActivityonGame();
+	pt.duty();
+	
+}
+}
