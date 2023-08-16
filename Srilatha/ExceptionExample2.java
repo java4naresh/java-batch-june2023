15358fd986e28843eb7c12cd2f6b9a14e3116c14
srilatha <srilathabangaru14@gmail.com>
2023-08-16 18:39:43 +0530

    Exception

---
+++ Srilatha/ExceptionExample2.java
@@ -0,0 +1,8 @@
+public class ExceptionExample2 {
+ 
+   public static void main (String[] args) {
+  
+     String name = null;
+     name.charAt(0);
+}
+}
---
+++ Srilatha/ExceptionExample3.java
@@ -0,0 +1,8 @@
+public class ExceptionExample3 {
+ 
+   public static void main (String[] args) {
+  
+     String name = "";
+     name.charAt(0);
+}
+}
