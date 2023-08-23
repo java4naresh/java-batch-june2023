cc1f458b66e9052b3064cb908cd9f83825b2fed7
srilatha <srilathabangaru14@gmail.com>
2023-08-23 22:26:43 +0530

    filesPractice

--- Srilatha/java/src/Files/CreatingFolder.java
+++ Srilatha/java/src/Files/CreatingFolder.java
@@ -6,8 +6,40 @@ public class CreatingFolder {
 
 	public static void main(String[] args) {
 		//makeDirectory("srilatha");
-		readFileNames("D:\\NewFilesFolder");
+		//readFileNames("D:\\Srilatha");
+		checkAndCreateFile("D:/Srilatha","Bangaru");
 	}
+	
+	public static void checkAndCreateFile(String dir, String fileName) {
+		try {
+			File file=new File(dir);
+			if(file.isDirectory()) {
+				String[] fileNames=file.list();
+				boolean flag=false;
+				for(String fileName1:fileNames) {
+					if(fileName1.equals(fileName)) {
+						flag=true;
+					}
+				}
+			if(flag) {
+				System.out.println("File Already Available");
+			}else {
+				CreateFile(dir+"\\"+fileName);
+			}
+			
+			}else {
+			throw new InvalidDirectory_Exception("please provide valid Directory");
+		}
+	}catch(Exception e) {
+		e.printStackTrace();
+		
+	}finally {
+	}
+	}
+	
+	
+	
+
 	public static void readFileNames(String directory) {
 		try {
 			File file=new File(directory);
@@ -16,6 +48,10 @@ public class CreatingFolder {
 				for(String fileName:fileNames) {
 					System.out.println(fileName);
 				}
+			}else {
+				Boolean flag=file.mkdir();
+				if(flag)System.out.println("folder is created Succesfully");
+				else System.out.println("folder is not created Succesfully");
 			}
 			
 		}catch(Exception ae) {
@@ -39,9 +75,9 @@ public class CreatingFolder {
 	}
 	}
 
-	public static void CreateFile() {
+	public static void CreateFile(String name) {
 		try {
-			File file=new File("src\\Files\\Srilatha.txt");
+			File file=new File(name);
 			boolean flag=file.createNewFile();//File creation
 			if(flag==true)System.out.println("File created Successfully");
 			else System.out.println("File not created Successfully");
